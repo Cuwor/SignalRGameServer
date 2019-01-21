e85376d6ad5e99ed2439d6faf2d6563f47afa4f8
Толстиков Артем <temichit@yandex.ru>
2019-01-21 09:25:42 +0700

    First chat commit

---
+++ SignalRChat/ChatHub.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+namespace SignalRChat
+{
+    public class MyHub : Hub
+    {
+        public void Send(string name, string message)
+        {
+            // Call the broadcastMessage method to update clients.
+            Clients.All.broadcast(name, message);
+        }
+    }
+}
